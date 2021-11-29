352ab68e433365c80ff31eee9714f9450bf6d672
Thon Ly <thonly@heartbank.ceo>
2021-11-29 10:22:50 -0800

    version 2

--- russian.js
+++ russian.js
@@ -2,3 +2,9 @@ function updateLanguage(event) {
     //console.log(event.target.value)
     document.location = event.target.value
 }
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-46787613-3');
